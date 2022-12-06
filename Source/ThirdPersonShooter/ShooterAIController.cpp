fcf8a0f8031e36b2412fac4e3c905d3f63be9362
Sebastian Yang <47506432+SCP650@users.noreply.github.com>
2022-12-05 22:47:52 -0500

    added nav mesh + enemy chase

---
+++ Source/ThirdPersonShooter/ShooterAIController.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	SetFocus(PlayerPawn);
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	MoveToActor(PlayerPawn, 200, false);
+}
