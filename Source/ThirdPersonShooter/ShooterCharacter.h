6a1aa8e37f18f73a36e9bf1d769714b136ba506e
Sebastian Yang <47506432+SCP650@users.noreply.github.com>
2022-12-14 22:21:35 -0500

    added health bar

--- Source/ThirdPersonShooter/ShooterCharacter.h
+++ Source/ThirdPersonShooter/ShooterCharacter.h
@@ -24,6 +24,8 @@ protected:
 public:	
 	UFUNCTION(BlueprintPure)
 		bool IsDead() const;
+	UFUNCTION(BlueprintPure)
+		float GetHealthPercent() const;
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
